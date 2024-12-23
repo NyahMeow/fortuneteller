ceb4901917aeeedd477716ef0b8dfaff3494aa50
NyahMeow <154108203+NyahMeow@users.noreply.github.com>
2024-12-23 14:28:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
 function checkFortune() {
-   var colour = document.getElementById('colour-select').value;
-   var number = parseInt(document.getElementById('number-input').value, 10);
-   var result = "凶"; // デフォルトの値
-                
-      if (colour === "黒" && number === 0) {
-             result = "小吉";
-      } else if (colour === "黒" && number === 1) {
-             result = "小吉";
-      } else if (colour === "黒" && number === 2) {
-             result = "小吉";
-      } else if (colour === "白" && number === 0) {
-             result = "小吉";
-      } else if (colour === "白" && number === 1) {
-             result = "小吉";
-      } else if (colour === "白" && number === 2) {
-             result = "凶";
-      } else if (colour === "オレンジ" && number === 0) {
-             result = "凶";
-      } else if (colour === "オレンジ" && number === 1) {
-             result = "小吉";
-      } else if (colour === "オレンジ" && number === 2) {
-             result = "小吉";
-      }
+    // 入力値を取得
+    const colour = document.getElementById('colour-select').value;
+    const number = parseInt(document.getElementById('number-input').value, 10);
 
-      document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
-      }
+    // 結果を格納する変数
+    let result = "凶"; // デフォルト値
 
-// 画像の表示処理
-    var imageContainer = document.getElementById('image-container');
+    // 条件による結果判定
+    if (colour === "黒" && number === 0) {
+        result = "小吉";
+    } else if (colour === "黒" && number === 1) {
+        result = "中吉";
+    } else if (colour === "黒" && number === 2) {
+        result = "吉";
+    } else if (colour === "白" && number === 0) {
+        result = "中吉";
+    } else if (colour === "白" && number === 1) {
+        result = "吉";
+    } else if (colour === "白" && number === 2) {
+        result = "凶";
+    } else if (colour === "オレンジ" && number === 0) {
+        result = "大吉";
+    } else if (colour === "オレンジ" && number === 1) {
+        result = "中吉";
+    } else if (colour === "オレンジ" && number === 2) {
+        result = "吉";
+    }
+
+    // 結果を表示
+    document.getElementById('result-output').innerText = `あなたの今週の運勢は【${result}】です`;
+
+    // 画像の切り替え
+    const imageContainer = document.getElementById('image-container');
     imageContainer.innerHTML = ""; // 既存の画像をクリア
 
-    var img = document.createElement('img');
+    const img = document.createElement('img');
     switch (result) {
-        case "小吉":
+        case "大吉":
             img.src = "images/moss.jpg";
             break;
+        case "凶":
+            img.src = "images/default.jpg";
+            break;
         default:
-            img.src = "images/default.jpg"; // デフォルト画像を設定
+            img.src = "images/moss.jpg";
             break;
     }
 
-    img.alt = result; // アクセシビリティのためにalt属性を設定
-    img.style.width = "200px"; // 必要に応じてスタイルを調整
+    img.alt = result; // アクセシビリティのためのalt属性
+    img.style.width = "300px"; // 必要に応じてスタイルを調整
     imageContainer.appendChild(img);
 }
-
