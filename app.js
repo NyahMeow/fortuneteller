23ca494dbd6545f849b220f9b139e97b68f6a97f
NyahMeow <154108203+NyahMeow@users.noreply.github.com>
2024-12-23 13:54:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
         function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "小吉"; // デフォルトの値
-
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
+            var result = "凶
             } else if (colour === "黒" && number === 2) {
                 result = "凶";
             } else if (colour === "白" && number === 0) {
@@ -35,8 +30,11 @@
         case "小吉":
             img.src = "images/moss.jpg";
             break;
-
+        default:
+            img.src = "images/default.jpg"; // デフォルト画像を設定
+            break;
 }
+
     img.alt = result; // アクセシビリティのためにalt属性を設定
     img.style.width = "200px"; // 必要に応じてスタイルを調整
     imageContainer.appendChild(img);
